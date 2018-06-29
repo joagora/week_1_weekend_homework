dc4c3c3d8e417985db3f0b554b61e9a27b48eafb
user <user@users-MacBook-Pro.local>
2018-06-29 11:13:08 +0100

    completed add_or_remove_cash test

--- start_point/pet_shop.rb
+++ start_point/pet_shop.rb
@@ -1,3 +1,10 @@
 def total_cash shop
   return shop[:admin][:total_cash]
 end
+
+
+def add_or_remove_cash__add shop, amount
+  total_cash = total_cash(shop)
+  total_cash += amount
+  return total_cash
+end
