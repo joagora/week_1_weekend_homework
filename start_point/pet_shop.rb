c30cb21d9d17820778f56175413f1dce100176b6
user <user@users-MacBook-Pro.local>
2018-06-29 15:47:10 +0100

    completed add_pet_to_stock test

--- start_point/pet_shop.rb
+++ start_point/pet_shop.rb
@@ -37,6 +37,8 @@ def find_pet_by_name shop, name
   for pet in pets
     if pet[:name] == name
       return pet
+    else
+      return nil
     end
   end
 end
@@ -48,4 +50,8 @@ def remove_pet_by_name shop, name
   pets = shop[:pets]
   pets.delete(pet_to_be_removed)
 
+end
+
+def add_pet_to_stock shop, pet
+  shop[:pets] << pet
 end
