a7546c75485d3207bce271b905d27a7602a82234
user <user@users-MacBook-Pro.local>
2018-06-29 11:29:36 +0100

    improved on add_remove_cash

--- start_point/pet_shop.rb
+++ start_point/pet_shop.rb
@@ -3,8 +3,6 @@ def total_cash shop
 end
 
 
-def add_or_remove_cash__add shop, amount
-  total_cash = total_cash(shop)
-  total_cash += amount
-  return total_cash
+def add_or_remove_cash shop, amount
+  shop[:admin][:total_cash] += amount
 end
